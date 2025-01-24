5dc8768ba2020673f552e37f248e56eb67847cce
Eduard <pironkoveduard2@gmail.com>
2025-01-24 12:47:58 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+
+def main():
+    name = "Едуард"
+    college = "КТГГ"
+    message = f"Всім привіт! Мене звати {name}, я навчаюся в коледжі {college}. Бажаю всім успіхів!"
+    print(message)
+
+if __name__ == "__main__":
+    main()
